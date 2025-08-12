b050a1c955d934788cc997bcf7541dbc6c17b359
Femi-Godpower <femigodpower@gmail.com>
2025-08-12 14:01:33 +0200

    Upload Restaurant De Garage showbuild [12-08-2025]

--- website-de-garage/vite.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwind from '@tailwindcss/vite'
-import svgr from 'vite-plugin-svgr'
-
-export default defineConfig({
-  plugins: [react(), tailwind(), svgr()],
-  resolve: { alias: { '@': '/src' } }
-})
